f9ee00eccaddb2764370261b0f2f42f28363f24b
Gleb Katsko <belgokstak@gmail.com>
2023-03-11 23:57:17 +0300

    page for viewing tours and their descriptions

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,24 +1,38 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar, NavLink} from "react-bootstrap";
-import {AGENCY_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, AGENCY_ROUTE, LOGIN_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
+import {useNavigate} from 'react-router-dom'
 
 const NavBar = observer(() => {
     const{user} = useContext(Context)
+    const navigate = useNavigate()
     return (
+
 
 <Navbar bg="dark" variant="dark">
     <Container>
         <NavLink style = {{color:'white'}} href = {AGENCY_ROUTE}>Wee</NavLink>
         {user.isAuth ?
             <Nav className= "ml-auto" style = {{color: 'white'}}>
-                <Button variant={"outline-light"}>Админ панель</Button>
-                <Button variant={"outline-light"} className= "ml-2">Войти</Button>
+                <Button
+                    variant={"outline-light"}
+                    onClick={() => navigate(ADMIN_ROUTE)}>Админ панель
+                </Button>
+                <Button
+                    variant={"outline-light"}
+                    onClick={() => navigate(LOGIN_ROUTE)}
+                    className= "ml-2">Выйти</Button>
             </Nav>
             :
-            <Nav className= "ml-auto" style = {{color: 'white'}}>
-                <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+            <Nav
+                className= "ml-auto"
+                style = {{color: 'white'}}>
+                <Button
+                    variant={"outline-light"}
+                    onClick={() => user.setIsAuth(true)}>Авторизация
+                </Button>
             </Nav>
         }
     </Container>
--- client/src/pages/TourPage.js
+++ client/src/pages/TourPage.js
@@ -1,10 +1,54 @@
 import React from 'react';
-
+import {Col, Container, Row, Image, Button, Card} from "react-bootstrap";
+import bigStar from "../assets/bigStar.png"
 const TourPage = () => {
+
+
+
+    const tour = {id: 1, name: 'Таинственная Испания', price: 1200, rating: 0, img: `https://bolerotour.ru/upload/pl_plugin_8/1_5b06645948270.jpg`}
+    const description =
+        [
+            {id:1, title:'Страна', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magnam adipisci doloribus aut! Vitae voluptates harum at nemo eum nostrum minus quidem illum iure. Rerum tempora fugiat dolor praesentium quidem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magnam adipisci doloribus aut! Vitae voluptates harum at nemo eum nostrum minus quidem illum iure. Rerum tempora fugiat dolor praesentium quidem'},
+            {id:2, title:'Города', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magnam adipisci doloribus aut! Vitae voluptates harum at nemo eum nostrum minus quidem illum iure.'},
+            {id:3, title:'Программа тура', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magnam adipisci doloribus aut! Vitae voluptates harum at nemo eum nostrum minus quidem illum iure.'},
+            {id:4, title:'Места отьезда', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magnam adipisci doloribus aut! Vitae voluptates harum at nemo eum nostrum minus quidem illum iure.'}
+    ]
     return (
-        <div>
-            Tours
-        </div>
+        <Container className="mt-3">
+            <Row>
+                <Col md = {4}>
+                    <Image width = {300} height = {300} src = {tour.img}/>
+                </Col>
+                <Col md = {4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{tour.name}</h2>
+                        <div
+                            className="d-flex align-items-center justify-content-center "
+                            style={{background: `url(${bigStar}) no-repeat center center `, width:240, height:240, backgroundSize: 'cover', fontSize: 64}}
+                        >
+                            {tour.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md = {4}>
+                    <Card
+                    className="d-flex flex-column align-items-center justify-content-around"
+                    style ={{width:300, height:300, fontSize: 32, border: '5px solid lightgray' }}
+                    >
+                        <h3>От: {tour.price} руб.</h3>
+                        <Button variant = {"outline-dark"}>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Описание тура:</h1>
+                {description.map((info, index) =>
+                    <Row key = {info.id} style = {{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title} : {info.description}
+                    </Row>
+                )}
+            </Row>
+                </Container>
     );
 };
 
