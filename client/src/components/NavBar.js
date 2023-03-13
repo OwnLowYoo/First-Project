ba7c15410cfcbf46a4f514c15bcf6cb9ac2a8f2f
Gleb Katsko <belgokstak@gmail.com>
2023-03-13 17:16:46 +0300

    authorization and registration with the client

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,6 +8,12 @@ import {useNavigate} from 'react-router-dom'
 const NavBar = observer(() => {
     const{user} = useContext(Context)
     const navigate = useNavigate()
+
+    const logOut = () => {
+        user.setUser({})
+        user.setIsAuth(false)
+    }
+
     return (
 
 
@@ -24,7 +30,7 @@ const NavBar = observer(() => {
                 </Button>
                 <Button
                     variant={"outline-light"}
-                    onClick={() => navigate(LOGIN_ROUTE)}
+                    onClick={() => logOut()}
                     className= "ml-2"
                 >
                     Выйти
@@ -36,7 +42,7 @@ const NavBar = observer(() => {
                 style = {{color: 'white'}}>
                 <Button
                     variant={"outline-light"}
-                    onClick={() => user.setIsAuth(true)}>Авторизация
+                    onClick={() =>navigate(LOGIN_ROUTE)}>Авторизация
                 </Button>
             </Nav>
         }
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import {Button, Card, Container, Form, NavLink, Row} from "react-bootstrap";
-import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
-import {useLocation} from "react-router-dom";
+import {AGENCY_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
+import {useLocation, useNavigate} from "react-router-dom";
+import {login, registration} from "../http/userAPI";
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
 
-const Auth = () => {
+const Auth = observer (() => {
+    const {user} = useContext(Context)
     const location = useLocation()
+    const navigate = useNavigate()
     const isLogin = location.pathname === LOGIN_ROUTE
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
 
+    const click = async () => {
+        try {
+            let data;
+            if (isLogin) {
+                data = await login(email,password);
+
+            } else {
+                data = await registration(email, password)
+            }
+            user.setUser(data)
+            user.setIsAuth(true)
+            navigate(AGENCY_ROUTE)
+        }
+        catch (e){
+            alert(e.response.data.message)
+        }
+
+    };
 
     return (
         <Container className = "d-flex justify-content-center align-items-center"
@@ -17,11 +42,17 @@ const Auth = () => {
                <Form className="d-flex flex-column">
                    <Form.Control
                        className="mt-3"
-                   placeholder="Введите email..."
+                       placeholder="Введите email..."
+                       value = {email}
+                       onChange={e => setEmail(e.target.value)}
+
                    />
                    <Form.Control
                        className="mt-3"
                        placeholder="Введите пароль..."
+                       value = {password}
+                       onChange={e => setPassword(e.target.value)}
+                       type = "password"
                    />
                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
                        {isLogin ?
@@ -34,6 +65,7 @@ const Auth = () => {
                        }
                        <Button
                            variant={"outline-success"}
+                           onClick={click}
                        >
                            {isLogin ? 'Войти' : 'Регистрация'}
                        </Button>
@@ -44,6 +76,6 @@ const Auth = () => {
            </Card>
         </Container>
     );
-};
+});
 
 export default Auth;
